538c0262e9e5fca3308482262a833b66dbc4cb4b
ThanhND <thanhnd@saisystem.vn>
2025-01-09 20:45:19 +0700

    ok

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#  Import th? vi?n c?n thi?t
 import streamlit as st
 import pandas
 
+# Thi?t l?p b? c?c trang web
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -21,14 +23,23 @@ Below you can find some of the apps I have build in Python. Feel to contact me!
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
+
+    # Hi?n th? 10 ?ng d?ng ??u ti?n
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code] ({row['url']})")
 
 with col4:
-    for index, row in df[:10].iterrows():
+    # Hi?n th? c?c ?ng d?ng c?n l?i
+    for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code] ({row['url']})")
