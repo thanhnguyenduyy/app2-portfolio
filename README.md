6cba6931aca159644428a9a22e3fd8c6b570d547
ThanhND <thanhnd@saisystem.vn>
2025-01-09 20:04:42 +0700

    README.md images\photo.png main.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # app2-portfolio
+
+# Run app
+streamlit run main.py
+
---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Ardit Sulce")
+    content = """
+    Hi, I am Ardit! I am a Python programmer, teacher, and founder of PythonHow. I graduated in 2013 with a Master of Science in Geospatial Technologies from the University of Muenster in Germany with a focus on using Python for remote sensing.
+I have worked with companies from various countries, such as the Center for Conservation Geography, to map and understand Australian ecosystems, image processing with the Swiss in-Terra, and performing data mining to gain business insights with the Australian Rapid Intelligence.
+    """
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps I have build in Python. Feel to contact me!
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
